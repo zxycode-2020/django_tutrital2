f4cfb24008868b7a3f602007013d1947adc84fb6
zhuxuanyu <1049939190@qq.com>
2020-01-29 15:46:41 +0800

    url学习

--- app01/views.py
+++ app01/views.py
@@ -4,13 +4,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    print(request.method)
-    print(request.POST)
     return HttpResponse("这个是首页--app01")
 
 
 def article(request, aid):
-    return HttpResponse('文章页ok--app01')
+    return HttpResponse('这是第{}篇文章'.format(aid))
 
 
 def test_url(request):
