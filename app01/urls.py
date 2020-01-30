6e425e826db6c7483676c403cdc203e014c4400c
zhuxuanyu <1049939190@qq.com>
2020-01-30 14:49:54 +0800

    请求

--- app01/urls.py
+++ app01/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from app01.views import index, article, test_url
+from app01.views import index, article, test_url, student, students
 
 urlpatterns = [
     path('index/', index),
     path('article/<str:aid>/', article),
     path('test_url/', test_url),
+    path('students/', students),             # 学生列表
+    path('student/<str:stu_id>/', student),  # 学生单个
 ]
--- app01/views.py
+++ app01/views.py
@@ -3,12 +3,41 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def index(request):
+def index(request, *args, **kwargs):
+    data = request.GET
+    print("data====", data)
     return HttpResponse("这个是首页--app01")
 
 
 def article(request, aid):
     return HttpResponse('这是第{}篇文章'.format(aid))
+
+
+def students(request):
+    # get_data = request.GET
+    # get_list = request.GET.getlist("id")
+    post_data = request.POST
+    method = request.method
+    # print("get_data===", get_data)
+    # print("get_list====", get_list)
+    print("post_data====", post_data)
+    print("method===", method)
+
+    return HttpResponse('学生列表----app01')
+
+
+def student(request, stu_id):
+    import json
+    body_data = request.body
+    json_data = ""
+    if body_data:
+        json_data = json.loads(body_data)
+    method = request.method
+    print("stu_id====", stu_id)
+    print("body_data====", body_data)
+    print("json_data====", json_data)
+    print("method===", method)
+    return HttpResponse('这是id为{}的学生'.format(stu_id))
 
 
 def test_url(request):
